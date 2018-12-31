e0390979a1af8fc68c1095db66acee2c655068a4
Orlando Reategui Diaz <orlandordiaz@gmail.com>
2018-12-31 13:11:26 -0600

    Add production config to __init__

--- myproject/__init__.py
+++ myproject/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from config import DevConfig
+from config import DevConfig, ProdConfig
 from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
